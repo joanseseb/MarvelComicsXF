8a6cb5e9104f052d6259c989d83bde9c4ed0bce9
joanseseb <joanseseb@gmail.com>
2023-08-01 18:51:34 +0200

    Add UITest project to solution

---
+++ UITest/AppInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.marvelcomicsxf").StartApp();
+
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
